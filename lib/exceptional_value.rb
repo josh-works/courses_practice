e8110894596f844fab62506908d515b36a4b0902
Josh Thompson <thompsonjoshd@gmail.com>
2018-10-15 11:22:18 -0600

    green test for ExceptionalValue

--- lib/exceptional_value.rb
+++ lib/exceptional_value.rb
@@ -1,3 +1,7 @@
 class ExceptionalValue
-  
+  attr_reader :raw_value, :reason
+  def initialize(raw_value, reason = "Unspecified")
+    @raw_value = raw_value
+    @reason = reason
+  end
 end
--- test/exceptional_value_test.rb
+++ test/exceptional_value_test.rb
@@ -1,4 +1,12 @@
 require_relative 'test_helper'
 
 class ExceptionalValueTest < MiniTest::Test
+  
+  def setup
+    @ev = ExceptionalValue.new("55 cycles")
+  end
+  
+  def test_exceptional_value_exists
+    assert @ev
+  end
 end
