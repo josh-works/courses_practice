162fff16b9326c6f4cdab02603561de1ae301b3c
Josh Thompson <thompsonjoshd@gmail.com>
2018-10-14 19:07:05 -0600

    tests and code for DurationHelper

--- lib/helpers/duration_helper.rb
+++ lib/helpers/duration_helper.rb
@@ -4,6 +4,14 @@ module DurationHelper
     case duration
     when Days
       "#{duration.quantity} glorious days"
+    when Weeks
+      "#{duration.quantity} delightful weeks"
+    when Months
+      "#{duration.quantity} gruling months"
     end
+  end
+  
+  def render_course_info
+    "#{self.name} (#{self.format_duration})"
   end
 end
--- test/course_test.rb
+++ test/course_test.rb
@@ -3,7 +3,9 @@ require_relative 'test_helper'
 class CourseTest < MiniTest::Test
   
   def setup
-    @math = Course.new("Math", Days[657])
+    @math = Course.new("Math", Days[33])
+    @bio = Course.new("Biology", Weeks[3])
+    @stats = Course.new("Statistics", Months[2])
   end
   
   def test_course_exists
@@ -13,12 +15,12 @@ class CourseTest < MiniTest::Test
   end
   
   def test_course_duration_in_days
-    math = Course.new("Math", Days[657])
+    math = Course.new("Math", Days[33])
     assert_instance_of Days, math.duration
   end
   
   def test_course_duration_in_weeks
-    math = Course.new("Math", Weeks[657])
+    math = Course.new("Math", Weeks[33])
     assert_instance_of Weeks, math.duration
   end
   
@@ -27,9 +29,23 @@ class CourseTest < MiniTest::Test
     assert_instance_of Months, math.duration
   end
   
-  def test_format_duration
+  def test_format_duration_days
     result = @math.format_duration
-    assert_equal "657 glorious days", result
+    assert_equal "33 glorious days", result
+  end
+  
+  def test_format_duration_weeks
+    result = @bio.format_duration
+    assert_equal "3 delightful weeks", result
+  end
+  
+  def test_format_duration_months
+    result = @stats.format_duration
+    assert_equal "2 gruling months", result
+  end
+  
+  def test_render_course_info
+    assert_equal "Math (33 glorious days)", @math.render_course_info
   end
 end
 
